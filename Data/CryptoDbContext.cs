0aab14014359e87fac1647db6337f91914ce5768
ZackHolby <zackholbrook44@gmail.com>
2021-05-21 19:32:46 -0400

    added DBcontext and files

---
+++ Data/CryptoDbContext.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace crypto_sentiment.Models {
+  public class CryptDbContext : DbContext {
+    public DbSet<Crypto> Currencies { get; set; }
+
+    public CryptoDbContext(DbContextOptions<CryptoDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder) {
+      base.OnModelCreating(builder);
+
+      builder.Entity<CryptoData>().HasData(
+        new {
+          date = DateTime.Now,
+          CurrencyID = 1,
+          currPrice = 1000,
+          symbol = "BTC",
+          slug = "Bitcoin",
+          market_cap = 10
+        }, new {
+          date =  DateTime.Now,
+          CurrencyID = 2,
+          currPrice = 2000,
+          symbol = "ETH",
+          slug = "Ethereum",
+          market_cap = 20
+        }, new {
+          date = DateTime.Now,
+          CurrencyID = 3,
+          currPrice = 3000,
+          symbol = "ADA",
+          slug = "Cardano",
+          market_cap = 10
+        }, new {
+          date = DateTime.Now,
+          CurrencyID = 4,
+          currPrice = 4000,
+          symbol = "LINK",
+          slug = "Chainlink",
+          market_cap = 10
+        }
+      );
+    }
+  }
+}
