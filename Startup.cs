f813ce92d6e5f15f4eec5df6eefb2652645168ff
ZackHolby <zackholbrook44@gmail.com>
2021-05-22 11:34:39 -0400

    added to configure services

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace crypto_sentiment
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<CryptoService>();
+            services.AddDbContext<CryptoDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
