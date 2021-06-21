987efabe592cf6cea3d3c7e8e97a0f205c0a7c91
Zachary Holbrook <zackholbrook44@gmail.com>
2021-06-21 14:01:29 -0400

    added some search fucntionality to hit db and repopulate page. working on routig

--- Models/Crypto.cs
+++ Models/Crypto.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using System;
 
 
@@ -13,6 +14,7 @@ namespace crypto_sentiment.Models {
         [Required]
         public double currPrice { get; set; }
 
+        
         [Required]
         public int currencyID { get; set; }
 
@@ -22,7 +24,6 @@ namespace crypto_sentiment.Models {
 
         public string slug { get; set; }
 
-        [Required]
         public string symbol { get; set; }
 
 
