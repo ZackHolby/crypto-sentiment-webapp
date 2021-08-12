836524964c2fee09072119278ec9a294f577a2ef
Zachary Holbrook <zackholbrook44@gmail.com>
2021-08-12 14:45:50 -0400

    fixup

--- obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
@@ -13,98 +13,98 @@ namespace crypto_sentiment
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 1 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 2 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 3 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 4 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 5 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 6 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 7 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 8 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 9 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using crypto_sentiment;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 10 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using crypto_sentiment.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 11 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 12 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using MudBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 13 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using Blazorise;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "C:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
+#line 14 "c:\Users\zackh\Coding\crypto-sentiment-webapp\_Imports.razor"
 using MoreLinq;
 
 #line default
